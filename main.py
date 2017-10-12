f11b341e22aa5d3673362acd11e5e83d27daf619
orphanedcouplet <oliverdayers@gmail.com>
2017-10-12 17:58:31 -0500

    it works! basic functionality: can post blog entries.

--- main.py
+++ main.py
@@ -18,10 +18,25 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/blog", methods=["POST", "GET"])
 def index():
-    return render_template("blogscroll.html")
+    # TODO make it post the thing you enter
+    if request.method == "POST":
+        entry_title = request.form["title"]
+        entry_body = request.form["body"]
+        new_entry = Blog(entry_title, entry_body)
+        db.session.add(new_entry)
+        db.session.commit()
+    
+    entries = Blog.query.all()
 
+    # TODO make error messages for if blog title or blog body is empty
+    # hint: look at user-signup
+    return render_template("blog.html", title="The Blog", entries=entries)
+
+@app.route("/newpost", methods=["POST", "GET"])
+def new_post():
+    return redirect("/blog")
 
 if __name__ == "__main__":
     app.run()
