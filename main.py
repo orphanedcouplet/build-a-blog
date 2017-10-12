3c1978a8140e2d59444f07a6728f899411bf2527
orphanedcouplet <oliverdayers@gmail.com>
2017-10-12 15:55:56 -0500

    added Blog class and basic html templates

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ app.config["DEBUG"] = True
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://build-a-blog:blog-a-build@localhost:8889/build-a-blog"
+db = SQLAlchemy(app)
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(280))
-    body = db.Column(db.String)
+    body = db.Column(db.String(10000000))
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("base.html")
+    return render_template("blogscroll.html")
+
+
+if __name__ == "__main__":
+    app.run()
