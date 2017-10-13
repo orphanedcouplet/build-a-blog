70be132cf3a3aee983ac585fe7e03ef1978ee65d
orphanedcouplet <oliverdayers@gmail.com>
2017-10-12 19:41:07 -0500

    separate page for new posts - i'm very excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -20,23 +21,53 @@ class Blog(db.Model):
 
 @app.route("/blog", methods=["POST", "GET"])
 def index():
-    # TODO make it post the thing you enter
-    if request.method == "POST":
-        entry_title = request.form["title"]
-        entry_body = request.form["body"]
-        new_entry = Blog(entry_title, entry_body)
-        db.session.add(new_entry)
-        db.session.commit()
     
-    entries = Blog.query.all()
+    entries = Blog.query.order_by(desc(Blog.id)).all()
 
-    # TODO make error messages for if blog title or blog body is empty
-    # hint: look at user-signup
     return render_template("blog.html", title="The Blog", entries=entries)
 
 @app.route("/newpost", methods=["POST", "GET"])
 def new_post():
-    return redirect("/blog")
+    # TODO make error messages for if blog title or blog body is empty
+    # hint: look at user-signup
+
+    if request.method == "POST":
+        entry_title = request.form["title"]
+        entry_title_error = ""
+        entry_body = request.form["body"]
+        entry_body_error = ""
+
+        # validate title
+        if not entry_title:
+            entry_title_error = "Your post must have a title!"
+        
+        # validate body
+        if not entry_body:
+            entry_body_error = "Your post must have text!"
+        
+        if not entry_title_error and not entry_body_error:
+        # TODO make it post the thing you entered
+            new_entry = Blog(entry_title, entry_body)
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect("/blog")
+        else:
+            return render_template(
+            "newpost.html", 
+            entry_title_error=entry_title_error, 
+            entry_body_error=entry_body_error
+            )
+
+
+    return render_template("newpost.html")
+
+    #TODO fix the following bug:
+    # "return redirect('/blog')" >> can't see form to enter a new entry
+    # "return render_template('newpost.html')" >> doesn't redirect to /blog after posting entry
+    # probably needs a conditional?
+    
+
+
 
 if __name__ == "__main__":
     app.run()
